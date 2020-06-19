f569d31dfcede583d47ff1bd6cddbd40dbfdb639
Crystal Wang <crystalwang@berkeley.edu>
2020-06-19 12:31:17 -0700

    updated stopwords

--- corpus-testing.py
+++ corpus-testing.py
@@ -4,7 +4,6 @@ from nltk.corpus import PlaintextCorpusReader
 import nltk
 
 # 100 most common English words, from Wikipedia
-
 common = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", 
 			"he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", 
 			"or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", 
@@ -15,20 +14,22 @@ common = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", 
 			"give", "day", "most", "us"]
 
 # stopwords from the nltk textbook
-stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
-				'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
-				'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
-				'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
-				'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
-				'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
-				'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
-				'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
-				'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
-				'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
-				'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
-				'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 
+				'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 
+				'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 
+				'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 
+				'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 
+				'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 
+				'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 
+				'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 
+				'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 
+				'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', 
+				"should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', 
+				"didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 
+				'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', 
+				"wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
-punctuation = [".", ",", ";", ":", "!", "?", '’', '“', '-', '”']
+punctuation = [".", ",", ";", ":", "!", "?", '’', '“', '—', '”', "-"]
 
 corpus_root = # the location of the text for me
 robot = PlaintextCorpusReader(corpus_root, 'robo.txt') # important: the txt file shouldn't have any weird characters for formatting!
@@ -51,11 +52,11 @@ def find_informative(frequency_distribution, n):
 	return informative
 
 # when I ran the program against a short story I wrote, this was the result for the 50 most common words:
-{'g': 36, 'robot': 20, '.”': 19, '?”': 16, 'father': 11, 'd': 11, 'mother': 11, 'sister': 9, 'liked': 9, 'robots': 8, 
-'thought': 8, 'wanted': 8, 'didn': 8, 'almost': 7, 'things': 7, '—': 7, 'got': 7, 'question': 7, 'felt': 7, ',”': 7, 'ever': 6, 
-'really': 6, 'fell': 6, 'asked': 6, 'ask': 6, 'operation': 6, 'consciousness': 6, 'took': 5, 'around': 5, 'family': 5, 
-'response': 5, 'never': 5, 'home': 5, 'face': 5, 'room': 5, 'love': 5, 'update': 4, 'industry': 4, 'knowledge': 4, 'years': 4, 
-'hand': 4, 'features': 4, 'options': 4, 'selection': 4, 'systems': 4, 'already': 4, 'many': 4, 'always': 4, 'change': 4, 'stop': 4}
+{'g': 36, 'robot': 20, '.”': 19, '?”': 16, 'father': 11, 'mother': 11, 'sister': 9, 'liked': 9, 'robots': 8, 'thought': 8, 
+'wanted': 8, 'almost': 7, 'things': 7, 'got': 7, 'question': 7, 'felt': 7, ',”': 7, 'ever': 6, 'really': 6, 'fell': 6, 'asked': 6, 
+'ask': 6, 'operation': 6, 'consciousness': 6, 'took': 5, 'around': 5, 'family': 5, 'response': 5, 'never': 5, 'home': 5, 'face': 5, 
+'room': 5, 'love': 5, 'update': 4, 'industry': 4, 'knowledge': 4, 'years': 4, 'hand': 4, 'features': 4, 'options': 4, 'selection': 4, 
+'systems': 4, 'already': 4, 'many': 4, 'always': 4, 'change': 4, 'stop': 4, 'said': 4, 'exactly': 4, 'hobbies': 4}
 
 print(find_informative(freq_dist, 50))
 
